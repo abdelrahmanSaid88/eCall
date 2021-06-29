9b795035d2fe7cb723bfa1ab4144902bb8f642fb
abdelrahman Said <abdelrahmanSaid88>
2021-06-29 05:11:07 +0200

    Medical history and location screens

---
+++ lib/LocationPermision/LocationPermision.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:location/location.dart';
+class locationpermision extends StatefulWidget{
+  static final ROUTE_NAME='location';
+  @override
+  _locationpermisionState createState() => _locationpermisionState();
+}
+
+class _locationpermisionState extends State<locationpermision> {
+  bool servicelocation=true;
+  PermissionStatus _permissionGranted;
+  LocationData _location;
+  void initState(){
+    super.initState();
+    checkLocationServices();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+  Future<void> checkLocationServices() async {
+    Location location = Location();
+    servicelocation = await location.serviceEnabled();
+    if (servicelocation) {
+      _permissionGranted = await location.hasPermission();
+
+      if (_permissionGranted == PermissionStatus.granted) {
+        //_location = await location.getLocation();
+        //print(_location.latitude.toString() + " " +
+        //_location.longitude.toString());
+        location.onLocationChanged.listen((LocationData currentLocation) {
+          print(currentLocation.latitude.toString() + " " + currentLocation.longitude.toString());
+        });
+
+      }
+      else {
+        _permissionGranted = await location.requestPermission();
+
+        if (_permissionGranted == PermissionStatus.granted) {
+          print('user allowed');
+        } else {
+          SystemNavigator.pop();
+        }
+      }
+    } else {
+      servicelocation = await location.requestService();
+      if (servicelocation) {
+        _permissionGranted == await location.hasPermission();
+        if (_permissionGranted == PermissionStatus.granted) {
+          print('user allowed before');
+        }
+        else {
+          _permissionGranted = await location.requestPermission();
+          if (_permissionGranted == PermissionStatus.granted) {
+            print('user allowed');
+          } else {
+            SystemNavigator.pop();
+          }
+        }
+      }
+      else {
+        SystemNavigator.pop();
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:ecall/LocationPermision/LocationPermision.dart';
+import 'package:ecall/MadicalHistory/MedicalHistory.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
-class MyThemData{
+class MyThemData {
   static var DarkGreen = Color.fromRGBO(20, 58, 65, 1.0);
   static var BackgroundColor = Color.fromRGBO(213, 216, 216, 1.0);
   static var Black = Color.fromRGBO(14, 14, 14, 1.0);
   static var White = Color.fromRGBO(250, 250, 250, 1.0);
-
 }
+
 void main() {
   runApp(MyApp());
 }
@@ -15,6 +18,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        locationpermision.ROUTE_NAME:(context)=>locationpermision(),
+        MedicalHistory.ROUTE_NAME:(context)=>MedicalHistory(),
+
+
+      },
+      initialRoute: MedicalHistory.ROUTE_NAME,
+    );
   }
 }
